d56706d98c8467bfe6271aef3dc5f798ec6365af
Jorge Aguilar <jorgeaguilar@backupcode.cl>
2023-12-25 18:22:50 -0300

    Vista Login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    //
+
+    public function index()
+    {
+        return view('login.index');
+    }
+
+    public function iniciarSesion(Request $request)
+    {
+        $credenciales = $request->only('nombre', 'password');
+
+        if (Auth::attempt($credenciales)) {
+            // Autenticación exitosa
+            return redirect()->intended('dashboard');
+        }
+
+        // Autenticación fallida
+        return back()->withErrors([
+            'email' => 'Las credenciales proporcionadas son incorrectas.',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index']);
+
+Route::post('iniciarSesion', [LoginController::class, 'iniciarSesion'])->name('login');
