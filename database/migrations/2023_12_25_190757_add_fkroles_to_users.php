b03caad93a6ee4dca9c72d1d29a08026012f14f6
Jorge Aguilar <jorgeaguilar@backupcode.cl>
2023-12-25 17:03:15 -0300

    alter table idrol en users

--- database/migrations/2023_12_25_190757_add_fkroles_to_users.php
+++ database/migrations/2023_12_25_190757_add_fkroles_to_users.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->unsignedBigInteger('idRol');
             $table->foreign('idRol')
                 ->references('id')
                 ->on('roles');
