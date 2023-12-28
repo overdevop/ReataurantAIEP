7c165c937768f1505285cc5af657241d78656886
Danilo Zúñiga <dazz5141@gmail.com>
2023-12-28 12:11:58 -0300

    se agredaron rutas de editar y eliminar. Se creó en las views un edir.blade.php y lo mismo para eliminar en productos, en mesas se agregó el delete.blade.php. Se agregó en los controladores los update y delete para producto y mesas.

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,6 @@ Route::get('/mesas/{mesa}', [MesaController::class, 'destroy'])->name('eliminarM
 Route::get('productos', [ProductoController::class, 'index'])->name('viewProductos')->middleware('auth');
 Route::get('crearProducto', [ProductoController::class, 'create'])->name('createProducto')->middleware('auth');
 Route::post('addProducto', [ProductoController::class, 'store'])->name('storeProducto')->middleware('auth');
-//Route::get('editarProducto{producto}', [ProductoController::class, 'edit'])->name('editProducto')->middleware('auth');
-//Route::put('updateProducto/{producto}', [ProductoController::class, 'update'])->name('updateProducto')->middleware('auth');
-
-Route::get('/productos/{producto}/editar', [ProductoController::class, 'edit'])->name('editarProducto')->middleware('auth');
+Route::get('/producto{producto}', [ProductoController::class, 'edit'])->name('editarProducto')->middleware('auth');
 Route::put('/productos/{producto}', [ProductoController::class, 'update'])->name('updateProducto')->middleware('auth');
 Route::get('/productos/{producto}', [ProductoController::class, 'destroy'])->name('eliminarProducto')->middleware('auth');
